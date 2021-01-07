72dac7291ae0da73296c4eda6b6d47c6ba88bf72
Tristan <tlsmarshall1@gmail.com>
2021-01-07 01:11:51 -0500

    Added keyboard input

--- src/rail_control/src/keyboard_teleop.cpp
+++ src/rail_control/src/keyboard_teleop.cpp
@@ -1,7 +1,53 @@
+//Used for reference
+//https://github.com/methylDragon/teleop_twist_keyboard_cpp/blob/master/src/teleop_twist_keyboard.cpp
+
 #include "ros/ros.h"
 #include "std_msgs/Float64.h"
+#include "geometry_msgs/Twist.h"
+
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+
 
 std_msgs::Float64 pitchval;
+
+
+////////// Included directly from reference, to be replaced later //////////
+
+char key(' ');
+
+// For non-blocking keyboard inputs
+int getch(void)
+{
+  int ch;
+  struct termios oldt;
+  struct termios newt;
+
+  // Store old settings, and copy to new settings
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+
+  // Make required changes and apply the settings
+  newt.c_lflag &= ~(ICANON | ECHO);
+  newt.c_iflag |= IGNBRK;
+  newt.c_iflag &= ~(INLCR | ICRNL | IXON | IXOFF);
+  newt.c_lflag &= ~(ICANON | ECHO | ECHOK | ECHOE | ECHONL | ISIG | IEXTEN);
+  newt.c_cc[VMIN] = 1;
+  newt.c_cc[VTIME] = 0;
+  tcsetattr(fileno(stdin), TCSANOW, &newt);
+
+  // Get the current character
+  ch = getchar();
+
+  // Reapply old settings
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+
+  return ch;
+}
+
+////////// Included directly from reference, to be replaced later //////////
+
 
 int main(int argc, char **argv)
 {
@@ -12,10 +58,15 @@ int main(int argc, char **argv)
     ros::Publisher pitch = n.advertise<std_msgs::Float64>("pitch_val", 1000);
 
     ros::Rate loop_rate(10);
-
+    printf("Press q to exit");
 
     while (ros::ok())
     {
+        key = getch();
+        printf("%c\n", key);
+        if (key == 'q'){
+            break;
+        }
         pitchval.data = 10;
         pitch.publish(pitchval);
 
